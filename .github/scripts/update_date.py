5244424dc4bf2e3dd975548bc6c23a41ddab10f1
mukmookk <mukmookk@gmail.com>
2023-09-22 19:28:57 +0900

    CI TEST

--- .github/scripts/update_date.py
+++ .github/scripts/update_date.py
@@ -1,17 +1,21 @@
 import os
 import subprocess
+import logging
 from datetime import date
+
+logging.basicConfig(level=logging.DEBUG)
 
 def get_modified_files():
     output = subprocess.check_output(
         ['git', 'diff-tree', '--no-commit-id', '--name-only', '-r', 'HEAD'],
         text=True
     )
-    print(output)
+    logging.debug(output)
     return output.strip().split('\n')
 
 def update_md_file(file_path):
     today = date.today().strftime("%m/%d/%Y")
+    logging.debug(f'Updating {file_path} with {today}')
     with open(file_path, 'r', encoding='utf-8') as file:
         content = file.readlines()
     
@@ -22,10 +26,11 @@ def update_md_file(file_path):
     
     with open(file_path, 'w', encoding='utf-8') as file:
         file.writelines(content)
+        logging.debug(f'Updated {file_path} with {today}')
 
 if __name__ == "__main__":
     modified_files = get_modified_files()
-    print(modified_files)
+    logging.debug(modified_files)
     for file in modified_files:
         if file.endswith('.md') and file.startswith('docs/'):
             update_md_file(file)
